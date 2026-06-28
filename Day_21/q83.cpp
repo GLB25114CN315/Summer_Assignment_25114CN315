b9405c6b57f0984cbcf08ce646f5070de4e3757b
Shubh0189 <kumarashok39746@gmail.com>
2026-06-28 18:24:55 +0530

    Day 21 Assignment

---
+++ Day_21/q83.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char str [100];
+    int vowels =0,consonants =0;
+
+    cout <<"Enter string :";
+    cin.getline(str,100);
+
+    for(int i=0; str[i]!='\0'; i++){
+        char ch =tolower(str[i]);
+
+        if (ch>='a'&& ch<='z'){
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='0'||ch=='u')
+            vowels++;
+            else
+            consonants++;
+        }
+    }
+    cout <<"vowels="<<vowels<<endl;
+    cout <<"Consonants="<<consonants;
+    return 0;
+}
